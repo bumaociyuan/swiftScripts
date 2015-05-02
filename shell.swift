55a70ba0b2bf7cefe0d738030847d6adf1cca4cb
bumaociyuan <zx19880528@gmail.com>
2015-05-02 23:53:05 +0800

    zx

---
+++ shell.swift
@@ -0,0 +1,19 @@
+#!/usr/bin/env  swift
+
+import Foundation
+let env = NSProcessInfo.processInfo().environment
+if let user = env["USER"] as? String {
+      println("Hello World, this is \(user.uppercaseString)")
+}
+
+let currentPath = NSFileManager.defaultManager().currentDirectoryPath
+println("The current directory is \(currentPath)")
+
+let contentsAtPath = NSFileManager.defaultManager().contentsOfDirectoryAtPath(currentPath, error: nil) as? [String]
+if let contents = contentsAtPath {
+
+for contentItem in contents
+{
+    println(contentItem)
+}
+}
